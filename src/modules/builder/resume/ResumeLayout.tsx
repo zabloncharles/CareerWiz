9e8247ee097ba8ad05683f59dcfc8a18fdeef3e1
zabloncharles <zabloncharles@gmail.com>
2025-03-24 23:31:27 -0400

    Fix: Export ITemplateStore interface and update type usage in ResumeLayout

--- src/modules/builder/resume/ResumeLayout.tsx
+++ src/modules/builder/resume/ResumeLayout.tsx
@@ -6,6 +6,7 @@ import { useResumeStore } from 'src/stores/useResumeStore';
 import { useTemplates } from 'src/stores/useTemplate';
 import { useThemes } from 'src/stores/themes';
 import { useZoom } from 'src/stores/useZoom';
+import { ITemplateStore } from 'src/stores/useTemplate';
 
 // TODO: need to define types
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -14,8 +15,8 @@ export let StateContext: Context<any> = createContext(null);
 export const ResumeLayout = () => {
   const resumeData = useResumeStore();
   const zoom = useZoom((state) => state.zoom);
-  const setTemplate = useTemplates((state) => state.setTemplate);
   const templateId = useTemplates((state) => state.activeTemplate.id);
+  const setTemplate = useTemplates((state: ITemplateStore) => state.setTemplate);
   const Template = AVAILABLE_TEMPLATES[templateId].component;
   const selectedTheme = useThemes((state) => state.selectedTheme);
   StateContext = createContext(resumeData);
--- src/stores/useTemplate.ts
+++ src/stores/useTemplate.ts
@@ -2,7 +2,7 @@ import { AVAILABLE_TEMPLATES } from 'src/helpers/constants';
 import { ITemplate, ITemplateContent } from 'src/helpers/constants/index.interface';
 import create from 'zustand';
 
-interface ITemplateStore {
+export interface ITemplateStore {
   availableTemplate: ITemplate;
   activeTemplate: ITemplateContent;
   setTemplate: (template: ITemplateContent) => void;
